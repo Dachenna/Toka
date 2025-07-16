5d1222a72a34e9b9bd0fe1b39f290e40792da720
David <davidinprograming@gmail.com>
2025-07-16 20:58:05 +0100

    new file:   toka/.gitignore 	new file:   toka/README.md 	new file:   toka/eslint.config.js 	new file:   toka/index.html 	new file:   toka/package-lock.json 	new file:   toka/package.json 	new file:   toka/public/vite.svg 	new file:   toka/src/App.jsx 	new file:   toka/src/Components/Navbar.jsx 	new file:   toka/src/assets/react.svg 	new file:   toka/src/index.css 	new file:   toka/src/main.jsx 	new file:   toka/vite.config.js

---
+++ toka/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-amber-400'>Hi</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ toka/src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar() {
+    const navItems = [
+        {
+            name: 'Home',
+            link : '/',
+        },
+        {
+            name: ''
+        }
+    ]
+  return (
+    <header>
+      <nav>
+
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
