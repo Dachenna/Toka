1ef95dd691ccde04c45d38b80625bb8c47017fdb
David <davidinprograming@gmail.com>
2025-07-17 00:47:47 +0100

    modified:   toka/src/Components/Navbar.jsx

--- toka/src/Components/Navbar.jsx
+++ toka/src/Components/Navbar.jsx
@@ -7,12 +7,22 @@ function Navbar() {
             link : '/',
         },
         {
-            name: ''
-        }
-    ]
+            name: 'Create',
+            link: '/create',
+        },
+        {
+            name: 'Trends',
+            link: '/trends',
+        },
+        {
+            name: 'Collections',
+            link: '/collections',
+        },
+    ];
+
   return (
     <header>
-      <nav>
+      <nav className='flex flex-col items-center justify-between p-3 gap-0.5 bg-gradient-to-r'>
 
       </nav>
     </header>
